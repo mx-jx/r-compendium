2d80e0602e055db99f56639053c982dc6f737481
mx-jx <mjstowe@kirby.unsw.edu.au>
2024-03-03 01:29:57 +1100

    init repo

---
+++ .Rprofile
@@ -0,0 +1,4 @@
+source("renv/activate.R")
+
+## RCompendium Credentials ----
+options(given = "MJ", family = "Stowe", email = "mjstowe@kirby.unsw.edu.au", orcid = "0000-0002-8640-6532")
---
+++ make.R
@@ -0,0 +1,31 @@
+#' r-compendium: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author MJ Stowe \email{mjstowe@kirby.unsw.edu.au}
+#' 
+#' @date 2024/03/03
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
